030bba5ad18f77788302cc0c6f8f9713e8fa5277
Dylan McCarthy <mccarthy.dylan@gmail.com>
2025-02-24 14:06:25 -0500

    Initial Commit

---
+++ HintsAndNotes/HintsAndNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HintsAndNotesApp.swift
+//  HintsAndNotes
+//
+//  Created by Dylan  on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct HintsAndNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HintsAndNotesTests/HintsAndNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  HintsAndNotesTests.swift
+//  HintsAndNotesTests
+//
+//  Created by Dylan  on 2/24/25.
+//
+
+import Testing
+@testable import HintsAndNotes
+
+struct HintsAndNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
