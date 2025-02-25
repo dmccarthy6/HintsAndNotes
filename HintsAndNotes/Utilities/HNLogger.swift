0dfaaed5ab7e0ce965b807b5bafcc8746efe8cd2
Dylan McCarthy <mccarthy.dylan@gmail.com>
2025-02-25 16:00:36 -0500

    chore: stub out logger

---
+++ HintsAndNotes/Utilities/HNLogger.swift
@@ -0,0 +1,29 @@
+//
+//  HNLogger.swift
+//  HintsAndNotes
+//
+//  Created by Dylan  on 2/25/25.
+//
+
+import OSLog
+
+struct HNLogger {
+    enum Category: String {
+        case captureSession
+        case imagePicker
+        case permissions
+    }
+
+    let subsystem = "com.hintsandnotes."
+    let category: Category
+    var logger: Logger {
+        Logger(subsystem: subsystem, category: category.rawValue)
+    }
+}
+
+enum Log {
+    static func debug(category: HNLogger.Category, message: String) {
+        let logger = HNLogger(category: category).logger
+        logger.log(level: .debug, "\(message)")
+    }
+}
