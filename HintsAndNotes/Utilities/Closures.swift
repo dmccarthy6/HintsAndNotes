2919d883e9a3862eac8b68227d829194a3e3ca9d
Dylan McCarthy <mccarthy.dylan@gmail.com>
2025-02-28 15:02:47 -0500

    chore: add closures utility

---
+++ HintsAndNotes/Utilities/Closures.swift
@@ -0,0 +1,11 @@
+//
+//  Closures.swift
+//  HintsAndNotes
+//
+//  Created by Dylan  on 2/28/25.
+//
+
+import Foundation
+
+typealias VoidClosure = (() -> Void)
+typealias OptionalClosure = (() -> Void)?
