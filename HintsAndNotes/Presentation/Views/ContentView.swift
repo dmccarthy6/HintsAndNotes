0996e5ac75c955f3a8bcd8a061b849b46e58479e
Dylan McCarthy <mccarthy.dylan@gmail.com>
2025-02-28 15:02:59 -0500

    refactor: content view

--- HintsAndNotes/Presentation/Views/ContentView.swift
+++ HintsAndNotes/Presentation/Views/ContentView.swift
@@ -8,29 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var viewModel = CapturePhotoViewModel()
 
     var body: some View {
-        VStack {
-            if let image = viewModel.image {
-                image
-                    .resizable()
-                    .frame(maxWidth: .infinity, idealHeight: 375)
-                Spacer()
-            } else {
-                CameraView(session: viewModel.sessionManager.session)
-                
-                Button {
-                    viewModel.takePhoto()
-                } label: {
-                    Text("Take Photo")
-                }
-            }
-
-        }
-            .task {
-                await viewModel.requestPermissionIfNeededAndConfigureSession()
-            }
+        CameraView()
     }
 }
 
