b16dc67c5a6c00c3e7a767cbb49eeeef272f4900
Dylan McCarthy <mccarthy.dylan@gmail.com>
2025-03-06 14:55:43 -0500

    feat: add constants file

---
+++ HintsAndNotes/Utilities/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  HintsAndNotes
+//
+//  Created by Dylan  on 3/6/25.
+//
+
+import Foundation
+import SwiftUI
+
+enum Icons {
+    static let chevronRight = Image(systemName: "chevron.right")
+    static let gearShape = Image(systemName: "gearshape")
+    static let photo = Image(systemName: "photo")
+}
